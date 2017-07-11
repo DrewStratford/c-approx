f4b018d81b0799764f8dfba23f5f4c38c4aa66b9
Drew Stratford <drewstratford@outlook.com>
2017-07-11 18:01:51 +1200

    Improved error display and linking.
    The build script now uses temp files for linking. Also no longer
    attempts to link a program that didn't compile.

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
-#!
+#!/bin/bash
 
-dist/build/CComp/CComp $1 > $1.asm && nasm -f elf32 -F dwarf $1.asm && gcc -m32 $1.o
+asmfile="$(mktemp)"
+objectfile="$(mktemp)"
+#dist/build/CComp/CComp $1 > $1.asm && nasm -f elf32 -F dwarf $1.asm && gcc -m32 $1.o
+if dist/build/CComp/CComp $1 > $asmfile
+  then nasm -f elf32 -F dwarf $asmfile -o $objectfile && gcc -m32 $objectfile
+  else echo "compilation failed"
+fi
+
+rm -f $asmfile
+rm -f $objectfile
